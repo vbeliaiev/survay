b6b16c788adab3dad4100ad827417e87fcec45bd
Vlad <v.belyaev@mobidev.biz>
2016-05-10 01:44:13 +0300

    add redirect after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path, notice: 'Signed up!'
+      login(user_params[:email],user_params[:password])
+      redirect_to quizzes_path, notice: 'Signed up!'
     else
       flash[:error] = 'Email or password was invalid'
       redirect_to :back
