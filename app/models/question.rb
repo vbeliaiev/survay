f1f2641b9662a7788ab58be1db92d7776e9938db
Oksana Dushenkivska <oksanka.blaze@gmail.com>
2016-05-20 20:25:27 +0300

    comparison

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :quiz
   has_many :variants, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :compares, through: :variants
 
   enum type: { has_one_answer: 0, has_multiple_answers: 1, comparison: 2 }
 
