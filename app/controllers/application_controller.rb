986044774b6949bfa7d27f00835a141400f2bae5
Vlad <v.belyaev@mobidev.biz>
2016-05-20 20:08:18 +0300

    Update attempts views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include CanCan::ControllerAdditions
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_back_or_to root_url, :alert => exception.message
